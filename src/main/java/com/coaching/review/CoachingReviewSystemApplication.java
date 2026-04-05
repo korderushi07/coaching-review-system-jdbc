265d3fda26bec270c2ae7e12714de6e0a34c44f7
Rushikesh Korde <rushi.korde07@gmail.com>
2026-04-06 01:40:18 +0530

    Added Spring Boot backend and HTML frontend

---
+++ src/main/java/com/coaching/review/CoachingReviewSystemApplication.java
@@ -0,0 +1,13 @@
+package com.coaching.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachingReviewSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachingReviewSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coaching/review/CoachingReviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coaching.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachingReviewSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
